5027376c152df9a829cc61c069b5387f2be246b3
Lacrimosa-GH <lacrimosa1457@gmail.com>
2026-02-01 12:11:19 +0100

    anims

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,48 +1,53 @@
 using System;
 using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
-public class PlayerMovement : MonoBehaviour
+public class PlayerController
+    : MonoBehaviour
 {
-    
+    private PlayerHide _playerHide;
     private Rigidbody2D _rigidbody2D;
     private Animator _animator;
     private input _input;
-    private bool crouched = false;
+
     [SerializeField] private float _moveSpeed;
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _input = GetComponent<input>();
         _animator = GetComponent<Animator>();
+        _playerHide = GetComponent<PlayerHide>();
     }
     
     private void FixedUpdate()
     {
+        Debug.Log(_input.Move);
         _rigidbody2D.linearVelocityX = _input.Move * _moveSpeed;
+        
         if (_input.Move > 0)
         {
             transform.localScale = new Vector3(1f, 1f, 1f); // facing right
+            
         }
         else if (_input.Move < 0)
         {
+            
             transform.localScale = new Vector3(-1f, 1f, 1f); // facing left
+            
         }
         
     }
 
+    private void UpdateAnim()
+    {
+        if (_playerHide.UpdateHide()) _animator.Play("Crouch");
+        
+        else if (_input.Move != 0) _animator.Play("Walk");
+        
+        else _animator.Play("Idle");
+    }
+
     private void Update()
     {
-        if (_rigidbody2D.linearVelocityX != 0)
-        {
-            _animator.SetTrigger("Walk");
-        }
-        else if (crouched)
-        {
-            _animator.SetTrigger("Crouch");
-        }
-        else
-        {
-            _animator.SetTrigger("Idle");
-        }
+        UpdateAnim();
     }
 }
