967d3b2799eac5eb84c99cb76fa8a5eca2fb8125
Lacrimosa-GH <lacrimosa1457@gmail.com>
2026-02-01 12:33:44 +0100

    Fmodmusic perpetual musci playing
    private void Awake()
       {
          if (instance == null)
          {
             instance = this;
             DontDestroyOnLoad(gameObject);
          }
          else
          {
             Destroy(gameObject);
          }
       }

--- Assets/Scripts/FMOD/FMOD Music.cs
+++ Assets/Scripts/FMOD/FMOD Music.cs
@@ -4,6 +4,19 @@ using FMOD.Studio;
 
 public class FMODMusic : MonoBehaviour
 {
+    public static FMODMusic instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     void Start()
     {
         RuntimeManager.PlayOneShot("event:/MainTheme");
